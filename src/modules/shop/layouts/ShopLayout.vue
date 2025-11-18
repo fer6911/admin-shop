e74cbbf6aa1c3d176a8d791cbbd2f10358884e50
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-11-17 19:28:23 -0500

    [feat] se crea el diseño y se organiza en sub carpetas

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Header Navbar -->
+  <top-menu />
+
+  <!-- Routerview -->
+  <router-view />
+
+  <!-- Footer -->
+  <custom-footer />
+</template>
+<script setup lang="ts">
+import TopMenu from '../components/TopMenu.vue';
+import CustomFooter from '../components/CustomFooter.vue';
+
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
+import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('@/modules/shop/views/HomeView.vue'),
+        },
+      ],
+    },
     // {
     //   path: '/about',
     //   name: 'about',
