f1eab6637bf3881555078ba1cc8611d10fecaf04
fer6911 <pablo6911@outlook.com>
2026-03-09 20:35:43 -0500

    first commit

---
+++ src/modules/auth/actions/check-auth.actions.ts
@@ -0,0 +1,38 @@
+import { tesloApi } from "@/api/tesloApi"
+import type { AuthResponse, User } from "../interfaces"
+import { isAxiosError } from "axios"
+
+interface CheckError {
+  ok: false
+}
+interface CheckSuccess {
+  ok: true,
+  user: User,
+  token: string
+}
+
+export const checkAuthAction = async (): Promise<CheckError | CheckSuccess> => {
+  try {
+    const localToken = localStorage.getItem('tokrn')
+    if (localToken && localStorage.length < 10) {
+      return { ok: false }
+    }
+
+    const { data } = await tesloApi.get<AuthResponse>('/auth/check-status')
+
+    return {
+      ok: true,
+      user: data.user,
+      token: data.token
+    }
+
+  } catch (error) {
+    if(isAxiosError(error) && error.response?.status === 401){
+      return {
+        ok: false
+      }
+    }
+
+    throw Error('No se puedo Verificar la sesión')
+  }
+}
